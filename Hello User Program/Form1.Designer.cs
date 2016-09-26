ff0918cbb27df4cf7d18987ae896be66ec646810
Jazz <jasmynpaige14@gmail.com>
2016-09-26 10:45:11 -0500

    complete question
    finished product

---
+++ Hello User Program/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Hello_User_Program
+{
+    partial class frmHelloUser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblGreeting = new System.Windows.Forms.Label();
+            this.btnClickHere = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblGreeting
+            // 
+            this.lblGreeting.AutoSize = true;
+            this.lblGreeting.Location = new System.Drawing.Point(47, 127);
+            this.lblGreeting.Name = "lblGreeting";
+            this.lblGreeting.Size = new System.Drawing.Size(0, 13);
+            this.lblGreeting.TabIndex = 0;
+            this.lblGreeting.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnClickHere
+            // 
+            this.btnClickHere.Location = new System.Drawing.Point(50, 55);
+            this.btnClickHere.Name = "btnClickHere";
+            this.btnClickHere.Size = new System.Drawing.Size(75, 23);
+            this.btnClickHere.TabIndex = 2;
+            this.btnClickHere.Text = "Click Here";
+            this.btnClickHere.UseVisualStyleBackColor = true;
+            this.btnClickHere.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmHelloUser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnClickHere);
+            this.Controls.Add(this.lblGreeting);
+            this.Name = "frmHelloUser";
+            this.Text = "Hello User Program";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblGreeting;
+        private System.Windows.Forms.Button btnClickHere;
+    }
+}
+
---
+++ Hello User Program/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hello_User_Program
+{
+    public partial class frmHelloUser : Form
+    {
+        public frmHelloUser()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rdoClickHere_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblGreeting.Text = "Good Morining Mr. Hardman!";
+        }
+    }
+}
